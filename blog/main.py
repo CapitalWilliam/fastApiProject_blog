60ece264dabed4bfc8b5a443f1629bccd67ff37a
CapitalWilliam <wzf_2023@outlook.com>
2023-01-26 01:29:35 +0800

    fixed-rename get_blogs_function's name from "all" to "show_all_blogs"

--- blog/main.py
+++ blog/main.py
@@ -44,7 +44,11 @@ async def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.delete("/blog/{blog_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def destroy(blog_id, db: Session = Depends(get_db)):
-    one = db.query(models.Blog).filter(models.Blog.id == blog_id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {blog_id} not found.")
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'done'
 
@@ -61,7 +65,7 @@ async def update(blog_id, request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 @app.get("/blog", response_model=List[schemas.ShowBlog])
-async def all(db: Session = Depends(get_db)):
+async def show_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
