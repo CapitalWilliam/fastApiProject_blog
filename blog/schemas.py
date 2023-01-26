e716422af80190011506324c29fd9d9ab815647b
CapitalWilliam <wzf_2023@outlook.com>
2023-01-26 12:58:19 +0800

    add function at schemas.py- refactor user and blog schemas classes like: UserBase User ShowUser BlogBase Blog ShowBlog
    fixed - add one param "user_id" to post./blog

--- blog/schemas.py
+++ blog/schemas.py
@@ -21,29 +21,50 @@ schemas在后端中通常指数据库架构。
 """
 __author__ = 'Capital_Wu'
 
+from typing import List
+
 from pydantic import BaseModel
 
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
 
 
-class ShowBlog(BaseModel):
-    title: str
-    body: str
+class Blog(BlogBase):
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class User(UserBase):
+    class Config:
+        orm_mode = True
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
 
     class Config:
         orm_mode = True
 
 
-class User(BaseModel):
+class ShowUserWithBlogs(ShowUser):
     name: str
     email: str
-    password: str
-class ShowUser(BaseModel):
-    name: str
-    email: str
+    blogs: List[Blog] = []
+
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
 
     class Config:
         orm_mode = True
