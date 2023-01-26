37dfbb1abacce426e003585b43efce0e64f304b0
CapitalWilliam <wzf_2023@outlook.com>
2023-01-26 12:58:46 +0800

    fixed-add space to models.py.

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ from .database import Base
 
 
 class Blog(Base):
-    __tablename__="blogs"
+    __tablename__ = "blogs"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
@@ -29,7 +29,7 @@ class Blog(Base):
 
 
 class User(Base):
-    __tablename__="users"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     email = Column(String)
