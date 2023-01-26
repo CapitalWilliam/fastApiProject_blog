ca906e66585e554cf6dc852f939a5300810d80c0
CapitalWilliam <wzf_2023@outlook.com>
2023-01-26 22:32:30 +0800

    fixed - add String length to sqlalchemy.Column&String

--- blog/models.py
+++ blog/models.py
@@ -21,8 +21,8 @@ from .database import Base
 class Blog(Base):
     __tablename__ = "blogs"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    body = Column(String)
+    title = Column(String(50))
+    body = Column(String(50))
     user_id = Column(Integer, ForeignKey('users.id'))
 
     creator = relationship("User", back_populates="blogs")
@@ -31,8 +31,8 @@ class Blog(Base):
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String)
-    password = Column(String)
+    name = Column(String(50))
+    email = Column(String(50))
+    password = Column(String(150))
 
     blogs = relationship("Blog", back_populates="creator")
