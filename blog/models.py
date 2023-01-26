fc091e9439f7234ca7b18ca490215441adaccfb5
CapitalWilliam <wzf_2023@outlook.com>
2023-01-26 12:53:28 +0800

    add function at models.py- add relaitionship between user and blog database models

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,9 @@
 """
 __author__ = 'Capital_Wu'
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 from .database import Base
 
 
@@ -21,9 +23,16 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    creator = relationship("User", back_populates="blogs")
+
+
 class User(Base):
     __tablename__="users"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     email = Column(String)
     password = Column(String)
+
+    blogs = relationship("Blog", back_populates="creator")
