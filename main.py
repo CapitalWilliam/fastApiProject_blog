1dae7e2348e10814ef415d2f4146d4a38f2424c5
CapitalWilliam <wzf_2023@outlook.com>
2023-01-26 22:29:54 +0800

    fixed - move main.py from ./blog to ./

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     main
+   Description :
+   Author :       Capital_Wu
+   date：          2023/1/25
+-------------------------------------------------
+   Change Activity:
+                   2023/1/25:
+-------------------------------------------------
+"""
+__author__ = 'Capital_Wu'
+
 from fastapi import FastAPI
+from blog import models
+from blog.database import engine
+
+from blog.routers import blog, user, anthentication
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+app.include_router(anthentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
